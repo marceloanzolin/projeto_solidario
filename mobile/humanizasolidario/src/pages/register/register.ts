424f392839a9af2455f6147797a24bdc2500c650
Evandro Gonçalves <evandrolvg@gmail.com>
2020-08-11 19:53:06 -0300

    Layout cadastro de instituição

--- mobile/humanizasolidario/src/pages/register/register.ts
+++ mobile/humanizasolidario/src/pages/register/register.ts
@@ -23,4 +23,29 @@ export class RegisterPage {
   login() {
     this.nav.setRoot(LoginPage);
   }
+
+  loadImageFromDevice(event) {
+    const file = event.target.files[0];
+  
+    const reader = new FileReader();
+  
+    reader.readAsArrayBuffer(file);
+  
+    reader.onload = () => {
+  
+      // get the blob of the image:
+      let blob: Blob = new Blob([new Uint8Array((reader.result as ArrayBuffer))]);
+  
+      // create blobURL, such that we could use it in an image element:
+      let blobURL: string = URL.createObjectURL(blob);
+      console.log(blobURL);
+  
+    };
+  
+    reader.onerror = (error) => {
+      console.log('Error on upload image');
+      //handle errors
+  
+    };
+  };
 }
