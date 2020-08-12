fa1c20700f07897eca07555179b04103c740885b
“Felipe <felipescherer.ads@gmail.com>
2020-08-12 13:05:37 -0300

    Adicionado cors na api

--- api/server.js
+++ api/server.js
@@ -2,7 +2,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 const PORT = process.env.PORT || 3000;
-
+const cors = require("cors");
+app.use(cors());
 app.use(bodyParser.json());
 
 app.use(bodyParser.urlencoded({ extended: true }));
