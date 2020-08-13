ef93b38d69e64007c0352120cf7efc98c2277489
marceloupf <marceloanzolin.mv@gmail.com>
2020-08-13 17:18:06 -0300

    Implementação  instituição

--- mobile/humanizasolidario/src/models/instituicao.ts
+++ mobile/humanizasolidario/src/models/instituicao.ts
@@ -1,16 +1,16 @@
 export class Instituicao {
-
-    dsemail: string;
-    dssenha: string;
-    nminstituicao: string;
-    dsendereco: string;
-  dsdescricao :  string;
-  dscep :  string;
-  dsfone1 : string;
-  dsfone2 :  string;
-  dslinkmaps :  string;
-  dsstatus : string;
-  logo :   string;
+  codinstituicao: string;
+  dsemail: string;
+  dssenha: string;
+  nminstituicao: string;
+  dsendereco: string;
+  dsdescricao: string;
+  dscep: string;
+  dsfone1: string;
+  dsfone2: string;
+  dslinkmaps: string;
+  dsstatus: string;
+  logo: string;
 
 
 }
--- mobile/humanizasolidario/src/services/instituicao-service.ts
+++ mobile/humanizasolidario/src/services/instituicao-service.ts
@@ -1,12 +1,12 @@
-import {Injectable} from "@angular/core";
-import {INSTITUICOES} from "./mock-instituicao";
+import { Injectable } from "@angular/core";
+import { INSTITUICOES } from "./mock-instituicao";
 //import { Http } from '@angular/http';
 import 'rxjs/add/operator/map';
 import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
 // import { api } from './config';
 import { Observable } from 'rxjs/Observable';
-import { map, catchError,retry } from 'rxjs/operators';
-import { Instituicao } from  '../models/instituicao';
+import { map, catchError, retry } from 'rxjs/operators';
+import { Instituicao } from '../models/instituicao';
 
 
 //import {throwError } from 'rxjs';
@@ -14,121 +14,62 @@ import { Instituicao } from  '../models/instituicao';
 //i//mport { Game } from '../models/game';
 
 @Injectable()
-export class InstituicaoService{
+export class InstituicaoService {
   private instituicoes: any;
   private API_URL = 'http://localhost:3000/instituicao';
   url: string;
 
-base_path = 'http://localhost:3000/instituicao';
+  base_path = 'http://localhost:3000/instituicao';
 
-	constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) { }
 
-// Get Game data
-getAll(): Observable<Instituicao>   {
- 
-  return this.http
-  .get<Instituicao>(this.base_path)
-  .pipe(
-    retry(2),
-    catchError(this.handleError)
-  )
-   
-}
+  // Get Game data
+  getAll(): Observable<Instituicao> {
 
+    return this.http
+      .get<Instituicao>(this.base_path)
+      .pipe(
+        retry(2),
+        catchError(this.handleError)
+      )
 
-   // return new Promise((resolve, reject) => {
-
-   
-
-    //  return this.http
- //     .get(this.base_path)
-     // .pipe(
-     //   retry(2),
-      //  catchError(this.handleError)
-     // )
-  // }
+  }
 
 
-   // this.http.get(url)
-   //   .subscribe((result: any) => {
-    //    resolve(result.json());
-    //  },
-     // (error) => {
-     //   reject(error.json());
-     // });
-  //});
-//}
+  getItem(id): Observable<Instituicao> {
 
-// Handle API errors
-//handleError(error: HttpErrorResponse) {
- // if (error.error instanceof ErrorEvent) {
-    // A client-side or network error occurred. Handle it accordingly.
-  //  console.error('An error occurred:', error.error.message);
- // } else {
-    // The backend returned an unsuccessful response code.
-    // The response body may contain clues as to what went wrong,
-   // console.error(
-   //   `Backend returned code ${error.status}, ` +
-    //  `body was: ${error.error}`);
- // }
-  // return an observable with a user-facing error message
- // return throwError('Something bad happened; please try again later.');
-//};
+    return this.http
+      .get<Instituicao>('http://localhost:3000/instituicao/' + id)
+      .pipe(
+        retry(2),
+        catchError(this.handleError)
+      )
 
-// Private
+  }
 
- // getAll() {
-//console.log(this.http.get(this.url));
+  getCampanha(codinstituicao): Observable<Instituicao> {
 
-//console.log(this.http.get(this.url).pipe(
-    //  map(this.extractData),
-    // catchError(this.handleError)
-  //  ));
-    
- // return this.http.get(this.url).pipe(
-  //  map(this.extractData),
-   // catchError(this.handleError)
-  //);
+    return this.http
+      .get<Instituicao>('http://localhost:3000/campanha/' + codinstituicao)
+      .pipe(
+        retry(2),
+        catchError(this.handleError)
+      )
 
-// return new Promise((resolve, reject) => {
+  }
 
-   // let url = this.API_URL;
 
-   // this.http.get(url)
-    //  .subscribe((result: any) => {
-     //   console.log(result);
-     // },
-     // (error) => {
-     //   reject(error.json());
-    //  });
-  //});
-
-   
-  //  return this.instituicoes;
- // new Promise((resolve, reject) => {
-   // let url = this.API_URL;
-  //  let url = this.API_URL + 'users/?per_page=10&page=' + page;
-
- //   this.http.get(url)
-     // .subscribe((result: any) => {
-     //   console.log(result);
-     //   resolve(result.json());
-    //  },
-    //  (error) => {
-     //   reject(error.json());
-    //  });
- /// });
-  //return null;
-
-  //}
+  remove(item) {
+    this.instituicoes.splice(this.instituicoes.indexOf(item), 1);
+  }
 
 
   private extractData(res: Response) {
     let body = res;
     return body || {};
   }
-  
-  private handleError (error: Response | any) {
+
+  private handleError(error: Response | any) {
     let errMsg: string;
     if (error instanceof Response) {
       const err = error || '';
@@ -139,18 +80,5 @@ getAll(): Observable<Instituicao>   {
     console.error(errMsg);
     return Observable.throw(errMsg);
   }
-  
-  
-  getItem(id) {
-    for (var i = 0; i < this.instituicoes.length; i++) {
-      if (this.instituicoes[i].id === parseInt(id)) {
-        return this.instituicoes[i];
-      }
-    }
-    return null;
-  }
 
-  remove(item) {
-    this.instituicoes.splice(this.instituicoes.indexOf(item), 1);
-  }
 }
